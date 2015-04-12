e7245dcdfe07e7e0dc361ec7a24ca2302f1d29fe
tonypai <tony77794@gmail.com>
2015-04-12 14:54:33 +0800

    實作攻擊扣血

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
+	public bool isEnemy = false;
 	public float hp;
 	public float maxHP = 100f;
 	public float atk = 1f;
 	public float spd = 1f;
 
+	Character target;
 	Animator anim;
 
 	void Start () {
 		hp = maxHP;
+
+		if (isEnemy)
+			target = GameObject.Find ("Player").GetComponent<Character> ();
+		else
+			target = GameObject.Find ("Enemy").GetComponent<Character> ();
 
 		anim = GetComponent<Animator> ();
 		InvokeRepeating ("Attack", 0f, 1f);
@@ -19,6 +26,7 @@ public class Character : MonoBehaviour {
 
 	void Attack () {
 		anim.SetTrigger ("attack");
+		target.AddHP (-atk);
 	}
 
 	void Hit () {
