d833797454fff1037a0c2086c75c524944350613
tonypai <tony77794@gmail.com>
2015-04-12 11:17:27 +0800

    定義角色數值方法同時跟UI同步

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float hp;
+	public float maxHP = 100f;
+	public float atk = 1f;
+	public float spd = 1f;
+
+	void Start () {
+		hp = maxHP;
+	}
+
+	void AddHP (float amt) {
+		if (hp + amt > maxHP) {
+			hp = maxHP;
+		}
+		else if(hp + amt < 0) {
+			hp = 0;
+		}
+		else {
+			hp += amt;
+		}
+	}
+
+	void AddATK (float amt) {
+		atk += amt;
+	}
+
+	void AddSPD (float amt) {
+		spd += amt;
+	}
+}
---
+++ Assets/Scripts/PlayerVars.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerVars : MonoBehaviour {
+
+	float hp, atk, spd;
+	Player player;
+
+	void Start () {
+		 player = GameObject.Find ("Player").GetComponent<Player> ();
+	}
+	
+	void Update () {
+		hp = player.hp;
+		atk = player.atk;
+		spd = player.spd;
+
+		transform.Find ("HP").GetComponent<Text> ().text = "HP:"+hp.ToString("F0");
+		transform.Find ("ATK").GetComponent<Text> ().text = "ATK:"+atk.ToString("F0");
+		transform.Find ("SPD").GetComponent<Text> ().text = "SPD:"+spd.ToString("F0");
+	}
+}
