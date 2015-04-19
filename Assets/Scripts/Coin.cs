47a25e482b41e1783558ea2341188249254c414b
tonypai <tony77794@gmail.com>
2015-04-19 09:55:49 +0800

    金幣最大值最小值判定、實作賺金幣機制

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -13,4 +13,16 @@ public class Coin : MonoBehaviour {
 	void Update () {
 		GetComponent<Text> ().text = "COIN:" + coin;
 	}
+
+	void Add (int amt) {
+		if (coin + amt > 999999) {
+			coin = 999999;
+		}
+		else if (coin + amt < 0) {
+			coin = 0;
+		}
+		else {
+			coin += amt;
+		}
+	}
 }
---
+++ Assets/Scripts/EarnCoin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EarnCoin : MonoBehaviour {
+
+	int answer = 0;
+	
+	void Start () {
+		InvokeRepeating ("RandomNumber", .1f, 1f);
+	}
+	
+	void RandomNumber () {
+		GetComponent<Text>().text = Random.Range (0, 10).ToString();
+	}
+}
