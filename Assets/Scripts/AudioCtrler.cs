500681d915a3c3b5ac52ead15c9c766284c8686e
tonypai <tony77794@gmail.com>
2015-04-19 16:22:33 +0800

    上音效

---
+++ Assets/Scripts/AudioCtrler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioCtrler : MonoBehaviour {
+
+	public AudioClip[] audioClips;
+	AudioSource audioSource;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Play (int id) {
+		audioSource.clip = audioClips[id];
+		audioSource.Play ();
+	}
+}
