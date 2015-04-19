a56b1cc12f61d1fec49a55ccf6f983554603ae17
tonypai <tony77794@gmail.com>
2015-04-19 10:34:58 +0800

    用相對較簡單的方式偵測按下數字鍵1-9

--- Assets/Scripts/EarnCoin.cs
+++ Assets/Scripts/EarnCoin.cs
@@ -11,23 +11,36 @@ public class EarnCoin : MonoBehaviour {
 	}
 
 	void Update () {
-		if (
-			(Input.GetKeyDown(KeyCode.Alpha0) && answer == 0) || 
-		    (Input.GetKeyDown(KeyCode.Alpha1) && answer == 1) ||
-		    (Input.GetKeyDown(KeyCode.Alpha2) && answer == 2) ||
-			(Input.GetKeyDown(KeyCode.Alpha3) && answer == 3) ||
-		    (Input.GetKeyDown(KeyCode.Alpha4) && answer == 4) ||
-		    (Input.GetKeyDown(KeyCode.Alpha5) && answer == 5) ||
-		    (Input.GetKeyDown(KeyCode.Alpha6) && answer == 6) ||
-		    (Input.GetKeyDown(KeyCode.Alpha7) && answer == 7) ||
-		    (Input.GetKeyDown(KeyCode.Alpha8) && answer == 8) ||
-		    (Input.GetKeyDown(KeyCode.Alpha9) && answer == 9)
-		) {
-			Coin.Add (1);
-			RandomNumber ();
+//		if (
+//			(Input.GetKeyDown(KeyCode.Alpha0) && answer == 0) || 
+//		    (Input.GetKeyDown(KeyCode.Alpha1) && answer == 1) ||
+//		    (Input.GetKeyDown(KeyCode.Alpha2) && answer == 2) ||
+//			(Input.GetKeyDown(KeyCode.Alpha3) && answer == 3) ||
+//		    (Input.GetKeyDown(KeyCode.Alpha4) && answer == 4) ||
+//		    (Input.GetKeyDown(KeyCode.Alpha5) && answer == 5) ||
+//		    (Input.GetKeyDown(KeyCode.Alpha6) && answer == 6) ||
+//		    (Input.GetKeyDown(KeyCode.Alpha7) && answer == 7) ||
+//		    (Input.GetKeyDown(KeyCode.Alpha8) && answer == 8) ||
+//		    (Input.GetKeyDown(KeyCode.Alpha9) && answer == 9)
+//		) {
+//			Coin.Add (1);
+//			RandomNumber ();
+//		}
+	}
+
+	void OnGUI () {
+		Event e = Event.current;
+		if (e.type == EventType.KeyDown)
+		{
+			int key = (int)e.keyCode - (int)KeyCode.Keypad0;
+			if (key == answer) {
+				Coin.Add (5);
+				RandomNumber ();
+			}
+			Event.current.Use();
 		}
 	}
-	
+
 	void RandomNumber () {
 		answer = Random.Range (0, 10);
 		GetComponent<Text>().text = answer.ToString();
