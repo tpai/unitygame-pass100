728d6d3b80692addf15cfaeca0ece7c4f8ae452e
tonypai <tony77794@gmail.com>
2015-04-26 10:34:20 +0800

    首頁製作、遮罩製作

---
+++ Assets/Scripts/GameStartButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameStartButton : MonoBehaviour {
+
+	public Animator maskAnim;
+
+	public void ShowMask () {
+		transform.Find ("Text").GetComponent<Text>().enabled = false;
+		maskAnim.SetBool ("show", true);
+		Invoke ("GoToGamePlay", 1.5f);
+	}
+
+	public void GoToGamePlay () {
+		Application.LoadLevel ("GamePlay");
+	}
+}
